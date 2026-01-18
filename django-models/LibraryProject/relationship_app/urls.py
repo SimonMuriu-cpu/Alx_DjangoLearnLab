ee27ae7ef3be3792749bf56967722c9f2c42f02b
Simon Muriu <muriusimon6@gmail.com>
2026-01-18 19:10:49 +0300

    Fixing errors in relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-    path('book/add/', add_book, name='add_book'),
-    path('book/<int:book_id>/edit/', edit_book, name='edit_book'),
-    path('book/<int:book_id>/delete/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
 
